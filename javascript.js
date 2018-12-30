23ffe5d93d883d3c7827f1f8d70f9f153ff1fc15
Extr4v3rT <42317458+Extr4v3rT@users.noreply.github.com>
2018-12-30 17:28:02 +0100

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,20 @@
+//function onmouseoverFb() {
+  const fbcircle = document.querySelector('.fbcircle');
+  const fb = document.querySelector('.fb');
+  fbcircle.classList.add('whiteicon');
+  fb.classList.add('blackicon');
+}
+
+
+//function onmouseoverIg() {
+  const igcircle = document.querySelector('.igcircle')
+  const ig = document.querySelector('.ig')
+  igcircle.classList.toggle('fa-inverse');
+  ig.classList.toggle('fa-inverse');
+}
+//function onmouseoverM() {
+  const mcircle = document.querySelector('.mcircle')
+  const m = document.querySelector('.m')
+  mcircle.classList.toggle('fa-inverse');
+  m.classList.toggle('fa-inverse');
+}
